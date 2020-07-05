5fd70f242da1e95142e573e21b45555be767c0c3
Lucas Lazamé <lucaslazame@gmail.com>
2020-07-05 19:07:38 -0300

    aula operadores

---
+++ operadores.js
@@ -0,0 +1,32 @@
+/* ===========================================================
+OPERADORES DE COMPARAÇÃO
+
+    >       Maior
+    <       Menor
+    >=      Maior igual a
+    <=      Menor igual a 
+    ==      Igual a
+    ===     Iqual e do mesmo tipo
+    !=      Diferente de 
+    !==     Diferente, inclusive do tipo
+
+==============================================================*/
+
+// DESAFIO 1
+const idade = 16
+// verificar se a pessoa é maior igual a 18 anos
+// se sim, deixar entrar, se não , bloquear a entrada
+if (idade >= 18){
+    console.log('Deixar entrar')
+}   else{
+    console.log('Bloquear entrada')
+}
+
+// se a pessoa tiver 17 anos
+// avisar para voltar quando fizer 18 anos
+if (idade === 17){
+    console.log('VOlte quando tiver 18 anos.')
+}
+
+
+
