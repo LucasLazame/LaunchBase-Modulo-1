6080a889a32f6f6246f3a343d59a103608823046
Lucas Lazamé <lucaslazame@gmail.com>
2020-07-05 19:20:06 -0300

    aula operadores lógicos

--- operadores.js
+++ operadores.js
@@ -28,5 +28,34 @@ if (idade === 17){
     console.log('VOlte quando tiver 18 anos.')
 }
 
+/* ====================================================
+    OPERADORES LÓGICOS
 
+    && "E" As duas condições devem ser verdadeiras
+        para que a condição final seja verdadeira.
+    || "OU" Uma das condições deve ser verdadeira
+        para que a condição final seja verdadeira.
+    ! "Não" Nega uma condição
 
+=====================================================*/
+
+// DESAFIO 1
+//const idade = 16
+// verificar se a pessoa é maior igual a 18 anos
+// se sim, deixar entrar, se não , bloquear a entrada
+if (!(idade >= 18) || idade == 17){
+    console.log('Bloquear a entrada')
+}   else{
+    console.log('Deixar entrar')
+}
+
+/* ====================================================
+    OPERADORES DE ARITMÉTICOS
+
+    * Multiplicação
+    / divisão
+    % Resto da divisão 
+    + Adição
+    - Subtração
+
+=====================================================*/
