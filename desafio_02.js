11fa9611ba6193e29108f2002441c6244e020dfc
Lucas Lazamé <lucaslazame@gmail.com>
2020-07-05 19:57:53 -0300

    cálculo de aposentadoria

---
+++ desafio_02.js
@@ -0,0 +1,6 @@
+// Cálculo de aposentadoria
+
+// Tempo de contribuição mínimo para homens é de 35 anos e, para mulheres, 30 anos
+// Utilizando a regra 85-95, a soma da idade com o tempo de contribuição do homem 
+// precisa ser de no mínimo 95 anos, enquanto a mulher precisa ter no mínimo 
+// 85 anos na soma
