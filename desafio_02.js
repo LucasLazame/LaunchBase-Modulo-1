c7b5a9025ee9a16a7dc4e3f7525d10c29e1991cd
Lucas Lazamé <lucaslazame@gmail.com>
2020-07-05 20:54:28 -0300

    codigo concluido

--- desafio_02.js
+++ desafio_02.js
@@ -4,3 +4,33 @@
 // Utilizando a regra 85-95, a soma da idade com o tempo de contribuição do homem 
 // precisa ser de no mínimo 95 anos, enquanto a mulher precisa ter no mínimo 
 // 85 anos na soma
+
+const nome = 'Lucas'
+const sexo = 'F'
+const idade = 60
+const contribuicao = 29
+const soma = idade + contribuicao
+
+if (sexo != 'F'){
+    if(contribuicao >= 35){
+        if(soma >= 95){
+            console.log(`O seu tempo de contribuição é de ${soma}, portanto você pode se aposentar!!`)
+        }else{
+            console.log(`O Seu tempo de constribuição foi de ${soma} menor que 95 anos, então você ainda não pode se aposentar!!`)
+        }
+        
+    } else{
+        console.log('Você tem menos de 35 anos de contribuição, logo não pode se aposentar')
+    }
+}   else{
+    if(contribuicao >= 30){
+        if(soma >= 85){
+            console.log(`O seu tempo de contribuição é de ${soma}, então você pode se aposentar!!`)
+        }else{
+            console.log(`O seu tempo de constribuição é de ${soma} menor que 85 anos, então você ainda não pode se aposentar.`)
+        }
+    } else{
+        console.log('Você tem menos de 30 anos de contribuição, logo não pode se aposentar.')
+    }
+}
+
