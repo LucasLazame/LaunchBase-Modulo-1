34bba0f4ce3bb1c5b1b160941c443c21af4b8c72
Lucas Lazamé <lucaslazame@gmail.com>
2020-07-05 22:47:49 -0300

    arrys

--- index.js
+++ index.js
@@ -2,26 +2,26 @@
 // das notas entre os alunos e envia 
 // mensagem do cálculo da média.
 
-const aluno01 = 'Lucas'
-const notaAluno01 = 9.8
+//const aluno01 = 'Lucas'
+//const notaAluno01 = 9.8
 
-const aluno02 = 'Mayk'
-const notaaluno02 = 10
+//const aluno02 = 'Mayk'
+//const notaaluno02 = 10
 
-const aluno03 = 'Fulano'
-const notaAluno03 = 2
+//const aluno03 = 'Fulano'
+//const notaAluno03 = 2
 
-const media = (notaAluno01 + notaaluno02 + notaAluno03)/ 3
+//const media = (notaAluno01 + notaaluno02 + notaAluno03)/ 3
 
 // Se a média for maior que 5, parabenizar a turma
 
-if (media > 5){
-    console.log(`A média foi de ${media}. Parabéns`)
-} else {
-    conseole.log(`Sua nota está abaixo da média`)
-}
+//if (media > 5){
+//    console.log(`A média foi de ${media}. Parabéns`)
+//} else {
+//    conseole.log(`Sua nota está abaixo da média`)
+//}
 
-console.log(media)
+//console.log(media)
 
 const alunos = [
     {
@@ -40,19 +40,19 @@ const alunos = [
 
 const nomeDeAlunos = ["Lucas", "Mayk", "Fulano"]
 
-const aluno01 = {
-    nome: "Lucas",
-    nota: 9.8
-}
+//const aluno01 = {
+//    nome: "Lucas",
+//    nota: 9.8
+//}
 
-const aluno02 = {
-    nome: "Diego",
-    nota: 10
-}
+//const aluno02 = {
+//    nome: "Diego",
+//    nota: 10
+//}
 
-const aluno03 = {
-    nome: "Fulano",
-    nota: 2
-}
+//const aluno03 = {
+//    nome: "Fulano",
+//    nota: 2
+//}
 
-const media = (aluno01.nota + aluno02.nota + aluno03.nota)/3
+//const media = (aluno01.nota + aluno02.nota + aluno03.nota)/3
